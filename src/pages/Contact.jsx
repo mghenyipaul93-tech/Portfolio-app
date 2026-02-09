f4b5bdae2d94b3d22e09fb7bd09da829e401e4a8
nromeok <nelsonromeook@gmail.com>
2026-02-09 20:22:09 +0300

    PS-5 Create Projects and Contact page

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,16 @@
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Me</h1>
+      <p>You can reach me through the following channels:</p>
+
+      <ul>
+        <li>Email: example@email.com</li>
+        <li>Phone: +254 700 000 000</li>
+        <li>GitHub: github.com/yourusername</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Contact;
