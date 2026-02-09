4c8e0c19b0403790238cc191f3d9e4bf524da8dd
paulmwasaru <mghenyipaul93@gmail.com>
2026-02-09 15:41:37 +0300

    PS-2: setup routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
+import Profile from './pages/Profile'
 function App() {
-  const [count, setCount] = useState(0)
+return (
+  <div>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <nav>
+<Link to="/">Home</Link> |
+      <Link to="/about">About</Link> |
+      <Link to="/projects">Projects</Link> |
+      <Link to="/contact">Contact</Link> |
+      <Link to="/profile">Profile</Link>
+    </nav>
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+
+  </div>
+ )
 }
 
 export default App
